8fd6c0afb1a60db1cd2a1c132a1a85aaf569ebe4
JOLeary5 <jonathanoleary2@knights.ucf>
2021-09-28 16:31:13 -0400

    Exercise 25 Outline

---
+++ exercise25/src/main/java/baseline/Solution25.java
@@ -0,0 +1,41 @@
+package baseline;
+
+//intasswordValidator(String Password){
+
+    // CHECK FOR # OF CHARACTERS IN THE PASSWORD STRING
+
+    // IF CHAR<8
+        //IF ALL NUMBERS
+            //RETURN 1
+        //IF ALL LETTERS
+            //RETURN 2
+
+    // IF CHAR>8
+        // IF THERE IS A MIX OF NUM AND LETTERS
+            //RETURN 3
+
+        //ELSE IF LETTERS NUM AND SPECIAL CHAR
+            //RETURN 4
+
+        //ELSE
+            //RETURN 2
+//}
+
+
+
+
+
+public class Solution25 {
+    //PROMPT FOR PASSWORD
+
+    //INT STRENGTH = passwordValidator
+
+    // IF 1
+        //VERY WEAK
+    // IF 2
+        //WEAK
+    // IF 3
+        //STRONG
+    // IF 4
+        //VERY STRONG
+}
