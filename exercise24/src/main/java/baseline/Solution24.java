51c9dbbc3a9d16c6d91a64eac7c16b4376a642c6
JOLeary5 <jonathanoleary2@knights.ucf>
2021-09-29 11:05:53 -0400

    exercise24 outline 9/29/2021

---
+++ exercise24/src/main/java/baseline/Solution24.java
@@ -0,0 +1,35 @@
+/*
+ * UCF COP3330 Fall 2021 Assignment 3 Solutions
+ * Copyright 2021 Jonathan O'Leary
+ */
+
+package baseline;
+import java.util.Scanner;
+
+public class Solution24 {
+
+        //void isAnagram {
+    //
+    // --check length of the two strings
+
+    //      -- if They are the same length --Else Not Anagram
+
+    //          -- sort the arrays
+
+    //          -- see if they are equal
+
+    //              -- IF EQUAL They are anagrams
+
+    //              -- ELSE Not anagrams
+    // }
+
+    public static void main(){
+
+        //prompt for strings
+
+        //send strings to function
+
+        //((FUNCTION))[is anagram]
+
+    }
+}
