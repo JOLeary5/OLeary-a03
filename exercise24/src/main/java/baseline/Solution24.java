b5e3e436b4e9ceb641b081209fbd32b0a346890a
JOLeary5 <jonathanoleary2@knights.ucf>
2021-09-29 12:39:34 -0400

    exercise 24 Completed 9/29/2021

--- exercise24/src/main/java/baseline/Solution24.java
+++ exercise24/src/main/java/baseline/Solution24.java
@@ -4,32 +4,47 @@
  */
 
 package baseline;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Solution24 {
+    static boolean isAnagram(String str1, String str2){
 
-        //void isAnagram {
-    //
-    // --check length of the two strings
+        String s1 = str1.replaceAll("\\s","");
+        String s2 = str2.replaceAll("\\s","");
 
-    //      -- if They are the same length --Else Not Anagram
+        boolean status = true;
+        if(s1.length()!=s2.length()){
+            status = false;
+        }
+        else{
+            char[] ArrayS1 = s1.toLowerCase().toCharArray();
+            char[] ArrayS2 = s2.toLowerCase().toCharArray();
+            Arrays.sort(ArrayS1);
+            Arrays.sort(ArrayS2);
+            status = Arrays.equals(ArrayS1,ArrayS2);
+        }
+        if (status){
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    public static void main(String[] args){
+        Scanner scanR = new Scanner(System.in);
 
-    //          -- sort the arrays
+        System.out.println("Enter two strings and ill tell you if they are anagrams: ");
+        System.out.print("Enter the first string: ");
+        String str1= scanR.nextLine();
 
-    //          -- see if they are equal
+        System.out.println("Enter the second string: ");
+        String str2= scanR.nextLine();
 
-    //              -- IF EQUAL They are anagrams
-
-    //              -- ELSE Not anagrams
-    // }
-
-    public static void main(){
-
-        //prompt for strings
-
-        //send strings to function
-
-        //((FUNCTION))[is anagram]
-
+        if (isAnagram(str1,str2))
+            System.out.println(str1 + " and "+ str2 + " are anagrams.");
+        else
+            System.out.println(str1 +" and "+ str2 + " are not anagrams");
     }
 }
