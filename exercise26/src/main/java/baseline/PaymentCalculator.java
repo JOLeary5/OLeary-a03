f8b4505cfd856754d8a0aea671e2310761518f29
JOLeary5 <jonathanoleary2@knights.ucf>
2021-09-29 16:05:38 -0400

    Outline of exercise 26

---
+++ exercise26/src/main/java/baseline/PaymentCalculator.java
@@ -0,0 +1,19 @@
+/*
+ * UCF COP3330 Fall 2021 Assignment 3 Solutions
+ * Copyright 2021 Jonathan O'Leary
+ */
+package baseline;
+import java.util.Scanner;
+
+public class PaymentCalculator {
+    public int calculateMonthsUntilPaidOff(){
+        // - prompt balance
+        //
+        // - prompt APR and take as a PERCENT
+        //
+        // - prompt monthly payment
+        // - make the fractions of a cent are rounded up
+        // - calculate months
+        //return months
+    }
+}
---
+++ exercise26/src/main/java/baseline/Solution26.java
@@ -0,0 +1,18 @@
+/*
+ * UCF COP3330 Fall 2021 Assignment 3 Solutions
+ * Copyright 2021 Jonathan O'Leary
+ */
+
+package baseline;
+
+
+class Solution26 {
+    public static void main(String[] args) {
+        // int months
+        //PaymentCalculator PaymentCalculatorObject = new PaymentCalculator();
+        //
+        // int months = PaymentCalculator class with a public method called calculateMonthsUntilPaidOf
+        //
+        // - result string display
+    }
+}
