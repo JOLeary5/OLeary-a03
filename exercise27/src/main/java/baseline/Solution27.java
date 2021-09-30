22e0d99331aa6aa13699c87d08adc8dd28569d46
JOLeary5 <jonathanoleary2@knights.ucf>
2021-09-29 21:35:50 -0400

    exercise27 Outline 2/29/2021

---
+++ exercise27/src/main/java/baseline/Solution27.java
@@ -0,0 +1,31 @@
+/*
+ * UCF COP3330 Fall 2021 Assignment 3 Solutions
+ * Copyright 2021 Jonathan O'Leary
+ */
+
+package baseline;
+
+public class Solution27 {
+        //void [VALIDATE_INPUT] function {
+    //
+    // CHECK TO MAKE SURE THAT THE FIRST NAME IS VALID
+    //
+    // CHECK TO SEE IF LAST NAME IS VALID
+    //
+    // CHECK TO SEE IF EMPLOYEE ID IS VALID
+    //
+    // CHECK TO SEE IF THE ZIP CODE IS VALID
+    //
+
+    public static void main(String[] args) {
+
+        //
+        //Prompt for first name
+        //Prompt for last name
+        //Prompt for Zip-code
+        //Prompt for employee ID
+        //
+        //call function Validate input
+        //end
+    }
+}
