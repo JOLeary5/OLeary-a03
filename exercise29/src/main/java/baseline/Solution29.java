1af49e90c83054a4466cee5e247f5bf53aa453da
JOLeary5 <jonathanoleary2@knights.ucf>
2021-09-30 13:51:04 -0400

    exercise29 Completed 9/30/2021

--- exercise29/src/main/java/baseline/Solution29.java
+++ exercise29/src/main/java/baseline/Solution29.java
@@ -3,17 +3,35 @@
  * Copyright 2021 Jonathan O'Leary
  */
 package baseline;
+import java.util.*;
 
 public class Solution29 {
-    public static void main(String[] args) {
+    public static void main(String[ ] args) {
 
+        Scanner sc=new Scanner(System.in);
+        boolean flag=false;
+        while(true)
+        {
+            try
+            {
+                System.out.print("What is the rate of return? "); //ask interest rate
+                int r=Integer.parseInt(sc.next()); //read r
+                System.out.println("It will take "+(72/r)+" years to double your initial investment.");
+                flag=true; //if exception not raised,set flag to true
+            }
+            catch(NumberFormatException e) //catch invalid number exception
+            {
+                System.out.println("Sorry. That's not a valid input");
+            }
+            catch(ArithmeticException e) //catch division by error exception
+            {
+                System.out.println("Sorry. That's not a valid input");
+            }
+            if(flag) //if exception not raised
+            {
+                break;
+            }
 
-
-        //
-        //     user number = Valid input class
-        //
-        //     print investment (userNumber/72)
-        //
-        //
+        }
     }
 }
