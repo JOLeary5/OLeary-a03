4c04c9feeab6348e4b55434683d46a7c20e4342f
JOLeary5 <jonathanoleary2@knights.ucf>
2021-09-30 14:18:48 -0400

    Exercise 30 Outline

---
+++ exercise30/src/main/java/baseline/Solution30.java
@@ -0,0 +1,17 @@
+/*
+ * UCF COP3330 Fall 2021 Assignment 3 Solutions
+ * Copyright 2021 Jonathan O'Leary
+ */
+package baseline;
+
+public class Solution30 {
+    public static void main(String[] args) {
+
+        // a for loop going to 12 [i]
+        //
+        // another FOR loop going to 12 [j]
+        // - - print i*j
+        //
+        // new line after 12 inputs
+    }
+}
