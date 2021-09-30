a9efdf9e1263b006a215b6a95e6fcf7d106b8656
JOLeary5 <jonathanoleary2@knights.ucf>
2021-09-30 14:25:00 -0400

    exercise30 Completed 9/30/2021

--- exercise30/src/main/java/baseline/Solution30.java
+++ exercise30/src/main/java/baseline/Solution30.java
@@ -7,11 +7,12 @@ package baseline;
 public class Solution30 {
     public static void main(String[] args) {
 
-        // a for loop going to 12 [i]
-        //
-        // another FOR loop going to 12 [j]
-        // - - print i*j
-        //
-        // new line after 12 inputs
+        for(int i=1; i<=12; i++){
+            for(int j=1; j<=12;j++){
+                System.out.print((i*j)+"\t");
+            }
+            System.out.println();
+        }
+
     }
 }
