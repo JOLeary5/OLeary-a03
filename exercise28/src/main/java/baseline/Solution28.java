0ed9d4129173b7c95a24cadbdec3a38e49051f10
JOLeary5 <jonathanoleary2@knights.ucf>
2021-09-30 10:43:03 -0400

    Outline for exercise 28

---
+++ exercise28/src/main/java/baseline/Solution28.java
@@ -0,0 +1,12 @@
+/*
+ * UCF COP3330 Fall 2021 Assignment 3 Solutions
+ * Copyright 2021 Jonathan O'Leary
+ */
+package baseline;
+
+public class Solution28 {
+    public static void main(String[] args) {
+        //FOR LOOP GOES TILL 5
+        //PRINT THE TOTAL OF THE 5 NUMBERS
+    }
+}
