290245118874c8e838f6c2f8bf739052bea0060f
JOLeary5 <jonathanoleary2@knights.ucf>
2021-09-30 10:51:37 -0400

    Exercise28 Completed 9/30/2021

--- exercise28/src/main/java/baseline/Solution28.java
+++ exercise28/src/main/java/baseline/Solution28.java
@@ -4,9 +4,18 @@
  */
 package baseline;
 
+import java.util.Scanner;
+
 public class Solution28 {
-    public static void main(String[] args) {
-        //FOR LOOP GOES TILL 5
-        //PRINT THE TOTAL OF THE 5 NUMBERS
+    public static void main(String[] args){
+        Scanner scanR = new Scanner(System.in);
+        int total=0, userInput;
+
+        for (int i=0; i<5; i++){
+            System.out.println("Enter a number: ");
+            userInput = scanR.nextInt();
+            total += userInput;
+        }
+        System.out.println("The total is "+ total);
     }
 }
