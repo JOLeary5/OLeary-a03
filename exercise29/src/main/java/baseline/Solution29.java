42d386c44625e561142952f96c7c12504b962789
JOLeary5 <jonathanoleary2@knights.ucf>
2021-09-30 13:11:58 -0400

    exercise 29 outline

---
+++ exercise29/src/main/java/baseline/Solution29.java
@@ -0,0 +1,19 @@
+/*
+ * UCF COP3330 Fall 2021 Assignment 3 Solutions
+ * Copyright 2021 Jonathan O'Leary
+ */
+package baseline;
+
+public class Solution29 {
+    public static void main(String[] args) {
+
+
+
+        //
+        //     user number = Valid input class
+        //
+        //     print investment (userNumber/72)
+        //
+        //
+    }
+}
---
+++ exercise29/src/main/java/baseline/validInput.java
@@ -0,0 +1,17 @@
+package baseline;
+import java.util.Scanner;
+
+public class validInput {
+    public static int validatedInput{
+        //A loop {
+        //      user inputs a value
+        //
+        //      - make sure its not 0
+        //
+        //      - No non numeric values
+        //
+        //      - returns the value user inputed
+        //      }
+    
+    }
+}
