9a5443d1f8eaba9723c5fa307dc7a0f35fbd914b
JOLeary5 <jonathanoleary2@knights.ucf>
2021-09-30 17:34:59 -0400

    exercise 31 outline completed

---
+++ exercise31/src/main/java/baseline/Solution31.java
@@ -0,0 +1,32 @@
+/*
+ * UCF COP3330 Fall 2021 Assignment 3 Solutions
+ * Copyright 2021 Jonathan O'Leary
+ */
+package baseline;
+
+public class Solution31 {
+    public static void PrintChart(int age,int restingHeart){
+        //
+        // - Display the top of the chart
+        //
+        // - FOR LOOP FROM 55 TO 95
+        //
+        //     - Display current loop counter(Intesnity)
+        //
+        //     - Display Rate
+        //
+        //     - keep formatting
+        //
+    }
+    public static void main(String[] args) {
+        //
+        // - Prompt for age
+        //
+        // - Prompt for restingHeartrate;
+        //
+        // - Display age RestingHeatrate
+        //
+        // - PrintChart(Age, RestingHeartrate)
+        //
+    }
+}
