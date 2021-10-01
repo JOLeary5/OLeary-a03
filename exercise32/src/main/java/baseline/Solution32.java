0e8d91e735058bbd416478b12bf1adf5c33bbd15
JOLeary5 <jonathanoleary2@knights.ucf>
2021-10-01 12:14:33 -0400

    exercise 32 Outline

---
+++ exercise32/src/main/java/baseline/Solution32.java
@@ -0,0 +1,79 @@
+/*
+ * UCF COP3330 Fall 2021 Assignment 3 Solutions
+ * Copyright 2021 Jonathan O'Leary
+ */
+package baseline;
+import java.util.Scanner;
+
+public class Solution32 {
+
+    // int ValidUserInpUt(){
+    //      - user enters a number
+    //      - exception throw if that user did not enter a number
+    //
+    //
+    //      - return user number
+    //
+    //}
+
+    // void random Number Game(difficulty){
+    //      - int guessCounter=0
+    //      - if (difficulty ==1)
+    //          - random number 1-10
+    //          - I HAVE MY NUMBER
+    //          - int guess == ValidUserInput
+    //          - while (false)
+    //                 guessCounter++
+    //                 if (guess<randomNumber)
+    //                      - "TOO LOW"
+    //                      - Enters again
+    //                 else if (guess>randomNumber)
+    //                      - TO HIGH
+    //                      - Enters again
+    //                 else
+    //                      - YOU GOT IT IN (guessCounter) Guesses
+    //                      - true BREAK
+    //          - if (difficulty ==2)
+    //              - random number 1-100
+    //              - I HAVE MY NUMBER
+    //              - int guess == ValidUserInput
+    //              - while (false)
+    //                     guessCounter++
+    //                     if (guess<randomNumber)
+    //                          - "TOO LOW"
+    //                          - Enters again
+    //                     else if (guess>randomNumber)
+    //                          - TO HIGH
+    //                          - Enters again
+    //                     else
+    //                          - YOU GOT IT IN (guessCounter) Guesses
+    //                          - true BREAK
+    //
+    //          - if (difficulty ==3)
+    //              - random number 1-1000
+    //              - I HAVE MY NUMBER
+    //              - int guess == ValidUserInput
+    //              - while (false)
+    //                     guessCounter++
+    //                     if (guess<randomNumber)
+    //                          - "TOO LOW"
+    //                          - Enters again
+    //                     else if (guess>randomNumber)
+    //                          - TO HIGH
+    //                          - Enters again
+    //                    else
+    //                         - YOU GOT IT IN (guessCounter) Guesses
+    //                          - true BREAK
+    //
+    
+
+    public static void main(String[] args) {
+        // "Lets Play a guessing game!"
+
+        // "Enter a difficulty
+        // int difficulty == ValidUserInput
+        // Check to make sure they entered a 1,2 or 3
+        //
+        //  randomNumberGame();
+    }
+}
