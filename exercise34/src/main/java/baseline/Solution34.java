ae8c5135eca547768f0d3ceeaa3fe98c2a2eeafc
JOLeary5 <jonathanoleary2@knights.ucf>
2021-10-01 13:54:04 -0400

    Exercise 34 Outline

---
+++ exercise34/src/main/java/baseline/Solution34.java
@@ -0,0 +1,23 @@
+/*
+ * UCF COP3330 Fall 2021 Assignment 3 Solutions
+ * Copyright 2021 Jonathan O'Leary
+ */
+
+package baseline;
+import java.util.Scanner;
+
+public class Solution34 {
+    public static void main(String[] args) {
+
+        //Establish the Scanner
+        //Establish an array of strings with employee names
+        //
+        // "ENTER NAME 2 DELETE"
+        //
+        // String delete = SCANNER.NEXT()
+        //
+        // remove delete string from array of strings
+        //
+        // Print the remaining names.
+    }
+}
