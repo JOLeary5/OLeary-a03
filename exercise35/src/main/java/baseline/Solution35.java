0d86f7a4bdbd67e0b6a854d587533e96da99da4b
JOLeary5 <jonathanoleary2@knights.ucf>
2021-10-01 18:20:20 -0400

    exercise35 completed 10/1/2021

--- exercise35/src/main/java/baseline/Solution35.java
+++ exercise35/src/main/java/baseline/Solution35.java
@@ -3,26 +3,29 @@
  * Copyright 2021 Jonathan O'Leary
  */
 package baseline;
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Random;
 
 public class Solution35 {
     public static void main(String[] args) {
-        // create an ArrayList
-        // initialize scanner 
-        //
-        // while loop to capture user inputs
-        //      - ENTER A NAME
-        //      - String userInput;
-        //     if (user input != blank)
-        //          - store the string in the user array
-        //     if (user input == blank
-        //          - break loop
-        //
-        //      int size = arrayList size
-        //
-        //      random number(MAX=ArrayListSize, MIN=0)
-        //
-        //      display the arrayobject=randomNumber
-        //
+        ArrayList<String>listOfNames = new ArrayList<String>();
+        Scanner scanR = new Scanner(System.in);
+        int check = 0;
+        while(check==0){
+            System.out.println("Enter a name: ");
+            String addString = scanR.nextLine();
+            if (addString.isBlank())
+                check = 1;
+            else {
+                listOfNames.add(addString);
+            }
+        }
+        int size= listOfNames.size();
+
+        Random random = new Random();
+        int randomNumber = random.nextInt(size);
+
+        System.out.println("The winner is ..."+listOfNames.get(randomNumber)+"!");
     }
 }
