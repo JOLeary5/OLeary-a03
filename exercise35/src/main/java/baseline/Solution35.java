ecf468e64c46d55367d63df80ae3fe06955f1a15
JOLeary5 <jonathanoleary2@knights.ucf>
2021-10-01 17:54:19 -0400

    exercise35 outline

---
+++ exercise35/src/main/java/baseline/Solution35.java
@@ -0,0 +1,28 @@
+/*
+ * UCF COP3330 Fall 2021 Assignment 3 Solutions
+ * Copyright 2021 Jonathan O'Leary
+ */
+package baseline;
+import java.util.Scanner;
+
+public class Solution35 {
+    public static void main(String[] args) {
+        // create an ArrayList
+        // initialize scanner 
+        //
+        // while loop to capture user inputs
+        //      - ENTER A NAME
+        //      - String userInput;
+        //     if (user input != blank)
+        //          - store the string in the user array
+        //     if (user input == blank
+        //          - break loop
+        //
+        //      int size = arrayList size
+        //
+        //      random number(MAX=ArrayListSize, MIN=0)
+        //
+        //      display the arrayobject=randomNumber
+        //
+    }
+}
