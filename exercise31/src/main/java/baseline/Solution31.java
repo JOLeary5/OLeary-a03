feb7eefc5a816c441a8ec4ef9312f12459af28bd
JOLeary5 <jonathanoleary2@knights.ucf>
2021-09-30 20:42:45 -0400

    Exercise31 Completed 9/30/2021

--- exercise31/src/main/java/baseline/Solution31.java
+++ exercise31/src/main/java/baseline/Solution31.java
@@ -4,29 +4,33 @@
  */
 package baseline;
 
+import java.util.Scanner;
+
 public class Solution31 {
     public static void PrintChart(int age,int restingHeart){
-        //
-        // - Display the top of the chart
-        //
-        // - FOR LOOP FROM 55 TO 95
-        //
-        //     - Display current loop counter(Intesnity)
-        //
-        //     - Display Rate
-        //
-        //     - keep formatting
-        //
+
+        float activeHeartRate=0;
+        System.out.println("Resting Pulse: "+restingHeart+"\t\t\t Age: "+age+"\n");
+        System.out.println("Intensity \t| Rate\t");
+        System.out.println("------------|--------------");
+        for (int i=55; i<100; i+=5){
+            float iStand = (float)i/100;
+            //System.out.println(iStand);
+            activeHeartRate = (((220 - age) - restingHeart) *iStand) + restingHeart;
+
+            System.out.println(i + "          | "+ Math.round(activeHeartRate)+ " bpm      ");
+
+        }
     }
     public static void main(String[] args) {
-        //
-        // - Prompt for age
-        //
-        // - Prompt for restingHeartrate;
-        //
-        // - Display age RestingHeatrate
-        //
-        // - PrintChart(Age, RestingHeartrate)
-        //
+        Scanner scanR = new Scanner(System.in);
+
+        System.out.println("Please enter your age");
+        int age = scanR.nextInt();
+        System.out.println("Please enter your resting heartRate");
+        int restingHeartRate = scanR.nextInt();
+
+        PrintChart(age, restingHeartRate);
+
     }
 }
