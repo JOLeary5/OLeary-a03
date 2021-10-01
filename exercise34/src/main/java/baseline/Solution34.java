365da546597a48664a4ba2d0f202f0b250632b6f
JOLeary5 <jonathanoleary2@knights.ucf>
2021-10-01 16:42:08 -0400

    exercise 24 completed 10/1/2021

--- exercise34/src/main/java/baseline/Solution34.java
+++ exercise34/src/main/java/baseline/Solution34.java
@@ -5,19 +5,36 @@
 
 package baseline;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Solution34 {
     public static void main(String[] args) {
 
-        //Establish the Scanner
-        //Establish an array of strings with employee names
-        //
-        // "ENTER NAME 2 DELETE"
-        //
-        // String delete = SCANNER.NEXT()
-        //
-        // remove delete string from array of strings
-        //
-        // Print the remaining names.
+        Scanner scanR = new Scanner(System.in);
+
+        ArrayList<String> names=new ArrayList();
+        names.add("John Smith");
+        names.add("Jackie Jackson");
+        names.add("Chris Jones");
+        names.add("Amanda Cullen");
+        names.add("Jeremy Goodwin");
+
+        int sizeOfArray = names.size();
+        System.out.println("There are "+ sizeOfArray + " employees:");
+        for (int i=0;i<sizeOfArray;i++){
+            System.out.println(names.get(i));
+        }
+        System.out.println("\n");
+        System.out.println("Enter the name you want to delete");
+        String delete = scanR.nextLine();
+        System.out.println("\n");
+
+        names.remove(delete);
+        sizeOfArray = names.size();
+        System.out.println("There are "+ sizeOfArray + " employees:");
+        for (int i=0;i<sizeOfArray;i++){
+            System.out.println(names.get(i));
+        }
+
     }
 }
