a425f4f83f895600cf0a04c44df742c9252618eb
JOLeary5 <jonathanoleary2@knights.ucf>
2021-10-01 13:11:01 -0400

    exercise 33 Outline

---
+++ exercise33/src/main/java/baseline/Solution33.java
@@ -0,0 +1,15 @@
+package baseline;
+
+public class Solution33 {
+    public static void main(String[] args) {
+        //create an array of string responses
+        //
+        //prompt user for question
+        //
+        // user enters a question
+        //
+        // random number 1-4
+        //
+        // match the random number with the string in the array
+    }
+}
