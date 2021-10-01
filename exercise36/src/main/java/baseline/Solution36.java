4bc79a8beff83895f740ce434a1eac0aea5af986
JOLeary5 <jonathanoleary2@knights.ucf>
2021-10-01 18:52:05 -0400

    exercise36 Outline

---
+++ exercise36/src/main/java/baseline/Solution36.java
@@ -0,0 +1,83 @@
+/*
+ * UCF COP3330 Fall 2021 Assignment 3 Solutions
+ * Copyright 2021 Jonathan O'Leary
+ */
+package baseline;
+import java.util.Scanner;
+
+public class Solution36 {
+
+    // int average(arrayList){
+    //  int average =0;
+    //  int size = sizeof.Arraylist
+    //
+    // for (size){
+    //      - Add array element to total
+    //      }
+    //
+    //    total=total/size
+    //
+    // return total
+    //}
+
+    //int max(arraylist){
+    //   int maxNum=0;
+    //   int size = sizeof.Arraylist
+    //   for (size){
+    //        if (arrayelement>MaxNum)
+    //            - maxNum = arrayElement;
+    //    }
+    //
+    //   return maxNum;
+    //}
+
+    //int man(arraylist){
+    //   int minNum=99999;
+    //   int size = sizeof.Arraylist
+    //   for (size){
+    //        if (arrayelement<MaxNum)
+    //            - minNum = arrayElement;
+    //    }
+    //
+    //   return minNum;
+    //}
+
+    //int std (arraylist){
+    // double sum =0;
+    // double sd = 0;
+    // dduble mean =0;
+    // int size=arraysize
+
+    //for (i=0: i<size; i++){
+    //  sum += array element
+    //
+    // mean = sum/n
+    // for(int i=0; i<size; i++){
+    //      sd = sd + Math.pow((arr.get(i) - mean),2);
+    //
+    // int std Math.sqrt(sd/n)
+
+
+    //MAIN{
+    //Initialize Scanner
+    //int check=0
+    //while(check==0){
+    //    - "ENTER A NUMBER"
+    //    - int userNumber = user input
+    //    - if (userNumber != done)
+    //         - add the number to array
+    //     - else
+    //          - check=1;
+    //
+    //
+    //  int average = average();
+    //  int max = max();
+    //  int min = min();
+    //  int std = std();
+    //
+    // - "THIS IS AVERAGE" + average
+    // - "THIS IS MAX" + max
+    // - "THIS IS MIN" + min
+    // - "THE STANDARD DEVIATION IS" + std
+    //}
+}
