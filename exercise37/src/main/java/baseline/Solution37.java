646b390e330b82cd6ab837072a754edd4e84b758
JOLeary5 <jonathanoleary2@knights.ucf>
2021-10-01 22:13:52 -0400

    exercise37 completed 10/1/2021

--- exercise37/src/main/java/baseline/Solution37.java
+++ exercise37/src/main/java/baseline/Solution37.java
@@ -3,38 +3,57 @@
  * Copyright 2021 Jonathan O'Leary
  */
 package baseline;
-import java.util.Scanner;
+import java.util.*;
 
 public class Solution37 {
     public static void main(String[] args) {
-        // - Scanner scanR = new Scanner(System.in);
-        // - "WHAT IS MINIMUM PASSWORD LENGTH
-        // - int size = user input
-        //
-        // - "HOW MANY SPECIAL CHARACTERS
-        // - int special = user input
-        //
-        // - 'HOW MANY NUMBERS"
-        // - int number = user input
-        //
-        //
-        // - declare a String of characters apperaently
-        // - declare a string of numbers
-        // - create random object
-        // - Arraylist of characters
-        // - LOOP FOR SPECIAL CHARACTERS{
-        //      - add special character
-        //}
-        // - LOOP FOR NUMBERS{
-        //       - add numbers to list
-        //
-        // - LOOP FOR CHARACTERS{
-        //       - add Characters for remaining length
-        //
-        // - shuffle the inputs
-        // - declare empty string
-        // - fill string with character elements
-        // - display password
-        //
+        Scanner scanR = new Scanner(System.in);
+        System.out.println("What's the minimum length? ");
+        int size = Integer.parseInt(scanR.nextLine());
+        int catchtest = 0;
+        int special = 0;
+        int numbers = 0;
+        while (catchtest==0){
+            System.out.println("How many special Characters? ");
+            special = Integer.parseInt(scanR.nextLine());
+
+            System.out.println("How many Numbers? ");
+            numbers = Integer.parseInt(scanR.nextLine());
+
+            if ((special+numbers)>(size/2)){
+                System.out.println("Sorry but the number of special characters and numbers has\n to be at least equal to the number of letters in your password\n");
+            }
+            else{
+                catchtest = 1;
+            }
+        }
+
+
+        String letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        String specialCharacters = "!@#$%^&*";
+        String numbersList = "1234567890";
+
+        Random random = new Random();
+        ArrayList<Character> chars = new ArrayList<>();
+
+        for(int i=0; i<special; i++){
+            chars.add(specialCharacters.charAt(random.nextInt(specialCharacters.length())));
+        }
+        for (int i=0; i<numbers; i++){
+            chars.add(numbersList.charAt(random.nextInt(numbersList.length())));
+        }
+
+        for (int i=chars.size()-1; i<size; i++){
+            chars.add(letters.charAt(random.nextInt(letters.length())));
+        }
+
+        Collections.shuffle(chars);
+        String randomPassword = "";
+
+        for(Character s : chars)
+        {
+            randomPassword += Character.toString(s);
+        }
+        System.out.println("Your password is: "+randomPassword);
     }
 }
