de3301a201a243be9febc74c3b31959c5f084bd0
JOLeary5 <jonathanoleary2@knights.ucf>
2021-10-01 21:17:47 -0400

    exercise36 Completed 10/1/2021

--- exercise36/src/main/java/baseline/Solution36.java
+++ exercise36/src/main/java/baseline/Solution36.java
@@ -3,81 +3,115 @@
  * Copyright 2021 Jonathan O'Leary
  */
 package baseline;
+
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.lang.*;
+import java.util.*;
 
 public class Solution36 {
+    static double average(ArrayList<Integer> NumArray){
+        double average =0;
+        int size = (NumArray.size());
+        if (size==0){
+            return 0;
+        }
+        for (int i=0; i<NumArray.size(); i++){
+            int number = NumArray.get(i);
+            average+=number;
+        }
+        average = average/size;
+        return average;
+    }
 
-    // int average(arrayList){
-    //  int average =0;
-    //  int size = sizeof.Arraylist
-    //
-    // for (size){
-    //      - Add array element to total
-    //      }
-    //
-    //    total=total/size
-    //
-    // return total
-    //}
+    static int max(ArrayList<Integer> NumArray){
+        int max=0;
+        int size = (NumArray.size());
+        if (size==0){
+            return 0;
+        }
+        for (int i=0; i<NumArray.size(); i++) {
+            if (max < NumArray.get(i)) {
+                max = NumArray.get(i);
+            }
+        }
+        return max;
+    }
 
-    //int max(arraylist){
-    //   int maxNum=0;
-    //   int size = sizeof.Arraylist
-    //   for (size){
-    //        if (arrayelement>MaxNum)
-    //            - maxNum = arrayElement;
-    //    }
-    //
-    //   return maxNum;
-    //}
+    static int min(ArrayList<Integer> NumArray){
+        int min=9999;
+        int size = (NumArray.size());
+        if (size==0){
+            return 0;
+        }
+        for (int i=0; i<NumArray.size(); i++) {
+            if (NumArray.get(i)<min) {
+                min = NumArray.get(i);
+            }
+        }
+        return min;
+    }
 
-    //int man(arraylist){
-    //   int minNum=99999;
-    //   int size = sizeof.Arraylist
-    //   for (size){
-    //        if (arrayelement<MaxNum)
-    //            - minNum = arrayElement;
-    //    }
-    //
-    //   return minNum;
-    //}
+    static double std(ArrayList<Integer> NumArray){
+        double sum =0;
+        double sd = 0;
+        double mean = 0;
+        int size = NumArray.size();
 
-    //int std (arraylist){
-    // double sum =0;
-    // double sd = 0;
-    // dduble mean =0;
-    // int size=arraysize
+        for (int i=0; i<size; i++){
+            sum = sum + NumArray.get(i);
+        }
 
-    //for (i=0: i<size; i++){
-    //  sum += array element
-    //
-    // mean = sum/n
-    // for(int i=0; i<size; i++){
-    //      sd = sd + Math.pow((arr.get(i) - mean),2);
-    //
-    // int std Math.sqrt(sd/n)
+        mean = sum/size;
 
+        for (int i=0; i<size; i++){
+            sd = sd + Math.pow((NumArray.get(i)-mean),2);
+        }
+        return Math.sqrt(sd/size);
+    }
+    public static void main(String[] args) {
+        ArrayList<Integer> NumArray = new ArrayList<>();
+        Scanner scanR = new Scanner(System.in);
+        int check=0;
+        int num = 0;
+        while(check==0) {
+            try {
+                System.out.println("Enter a number: ");
+                String userInput = scanR.nextLine();
+                if (userInput.equals("done")) {
+                    break;
+                } else {
+                    num = 1;
+                    for (int i = 0; i < userInput.length(); i++) {
+                        if (Character.isDigit(userInput.charAt(i))) {
+                            continue;
+                        } else {
+                            num = 0;
+                        }
+                    }
+                    if (num == 1) {
+                        NumArray.add(Integer.parseInt(userInput));
+                    }
 
-    //MAIN{
-    //Initialize Scanner
-    //int check=0
-    //while(check==0){
-    //    - "ENTER A NUMBER"
-    //    - int userNumber = user input
-    //    - if (userNumber != done)
-    //         - add the number to array
-    //     - else
-    //          - check=1;
-    //
-    //
-    //  int average = average();
-    //  int max = max();
-    //  int min = min();
-    //  int std = std();
-    //
-    // - "THIS IS AVERAGE" + average
-    // - "THIS IS MAX" + max
-    // - "THIS IS MIN" + min
-    // - "THE STANDARD DEVIATION IS" + std
-    //}
+                }
+            }
+            catch(Exception e)
+            {
+                System.out.println("Please enter an inter value");
+            }
+        }
+        System.out.println("Numbers: "+NumArray);
+
+        double average = average(NumArray);
+        int max = max(NumArray);
+        int min = min(NumArray);
+        double std =  std(NumArray);
+
+        System.out.println("The average is "+average);
+        System.out.println("The minimum is "+ min);
+        System.out.println("The maximum is "+ max);
+        System.out.println("The standard deviation is "+std);
+        //completeComment
+    }
+
 }
