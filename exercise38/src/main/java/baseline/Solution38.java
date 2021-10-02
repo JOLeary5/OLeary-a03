7f3aa6ba935be97d5986d460f8b37809fa3c5fc2
JOLeary5 <jonathanoleary2@knights.ucf>
2021-10-02 18:09:46 -0400

    Exercise 38 outline

---
+++ exercise38/src/main/java/baseline/Solution38.java
@@ -0,0 +1,32 @@
+/*
+ * UCF COP3330 Fall 2021 Assignment 3 Solutions
+ * Copyright 2021 Jonathan O'Leary
+ */
+package baseline;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Solution38 {
+
+        //String filterEvenNumbers(){
+        // if ( (EVEN%2)!= 0) {
+        //      - store in string array value to another even array
+        // return newString;
+
+
+    public static void main(String[] args) {
+
+        //initialize scanner
+        //prompt user to enter a list of numbers separated by spaces
+        //String user = String
+        //
+        //
+        //Convert string to array since apparently a lot of languages can do that
+        //
+        // String evenString = FUNCTION filterEvenNumbers();
+        //
+        // display evenString();
+        //
+
+    }
+}
