0ccd1cdadfd049587663eab5474ac2b4da74b37d
JOLeary5 <jonathanoleary2@knights.ucf>
2021-10-01 21:35:36 -0400

    exercise37 Outline

---
+++ exercise37/src/main/java/baseline/Solution37.java
@@ -0,0 +1,40 @@
+/*
+ * UCF COP3330 Fall 2021 Assignment 3 Solutions
+ * Copyright 2021 Jonathan O'Leary
+ */
+package baseline;
+import java.util.Scanner;
+
+public class Solution37 {
+    public static void main(String[] args) {
+        // - Scanner scanR = new Scanner(System.in);
+        // - "WHAT IS MINIMUM PASSWORD LENGTH
+        // - int size = user input
+        //
+        // - "HOW MANY SPECIAL CHARACTERS
+        // - int special = user input
+        //
+        // - 'HOW MANY NUMBERS"
+        // - int number = user input
+        //
+        //
+        // - declare a String of characters apperaently
+        // - declare a string of numbers
+        // - create random object
+        // - Arraylist of characters
+        // - LOOP FOR SPECIAL CHARACTERS{
+        //      - add special character
+        //}
+        // - LOOP FOR NUMBERS{
+        //       - add numbers to list
+        //
+        // - LOOP FOR CHARACTERS{
+        //       - add Characters for remaining length
+        //
+        // - shuffle the inputs
+        // - declare empty string
+        // - fill string with character elements
+        // - display password
+        //
+    }
+}
