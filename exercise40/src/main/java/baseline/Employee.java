6a011a109efe95247a973fc35aa72cb098ff337f
JOLeary5 <jonathanoleary2@knights.ucf>
2021-10-03 14:45:39 -0400

    exercise40 OutLine

---
+++ exercise40/src/main/java/baseline/Employee.java
@@ -0,0 +1,19 @@
+package baseline;
+
+public class Employee {
+    //String Firstname
+    //String Lastname
+    //String Position
+    //String Separation date
+
+    // EMPLOYEE DEFINITION (firstname, Lastname, Position, Separation Date)
+    // Get first name
+    // get last name
+    // get position
+    // get seperation date
+
+    //Get and Set firstname
+    //Get and Set LastName
+    //Get and Set Postion
+    //Get and Set Date
+}
---
+++ exercise40/src/main/java/baseline/Solution40.java
@@ -0,0 +1,13 @@
+package baseline;
+
+public class Solution40 {
+
+    // Establish the map list
+    //
+    // sout "Enter a name to search"
+    //    - User inputs string
+    //
+    // Use the string to search the map list
+    //
+    // display results from search in tabular format
+}
