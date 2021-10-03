7e9ee40b05d193ce8defcc1b606db3f571af6b34
JOLeary5 <jonathanoleary2@knights.ucf>
2021-10-03 09:40:48 -0400

    exercise39 Outline

---
+++ exercise39/src/main/java/baseline/Solution39.java
@@ -0,0 +1,25 @@
+/*
+ * UCF COP3330 Fall 2021 Assignment 3 Solutions
+ * Copyright 2021 Jonathan O'Leary
+ */
+package baseline;
+
+public class Solution39 {
+    public static void main(String[] args) {
+        //
+        // - Establish the  Structure DataArray
+        //      - add employee 1 (POSITION)(SEPARATION DATE)
+        //      - add employee 2 (POSITION)(SEPARATION DATE)
+        //      - add employee 3 (POSITION)(SEPARATION DATE)
+        //      - add employee 4 (POSITION)(SEPARATION DATE)
+        //      - add employee 5 (POSITION)(SEPARATION DATE)
+        //
+        //
+        // - Sort the Array by Last-Name
+        // - (THE FORMATTED TABLE)
+        //      - sout ( NAME   |    POSITION|      SEPARATION DATE
+        //      -  FOR (ArrayLength)
+        //      -       ( EMPLOYEE NAME)| (EMPLOYEE POSITION) | (SEPARATION DATE)
+
+    }
+}
